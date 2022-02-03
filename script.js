c309b2b882e2593eddb6f942d29b6e076680812c
faizahsayyid <faizah.sayyid@gmail.com>
2022-02-03 00:25:04 -0500

    responsive header

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const socials = document.getElementById("socials");
+const navList = document.getElementById("nav-list");
+let menuShow = false;
+
+hamburgerMenu.addEventListener("click", () => {
+  if (menuShow) {
+    socials.style.display = "none";
+    navList.style.display = "none";
+  } else {
+    socials.style.display = "block";
+    navList.style.display = "block";
+  }
+  menuShow = !menuShow;
+});
+
 const projectShowBtns = document.querySelectorAll("button[data-project-show]");
 
 projectShowBtns.forEach((projectShowBtn) => {
